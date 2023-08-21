80c3dd7bae36e20eb6ca5d5065c8699969c8927e
Shehroz <mshehroz302@gmail.com>
2023-08-21 20:53:00 +0500

    Add background & sign up page

--- src/Component/SignIn/Sign.tsx
+++ src/Component/SignIn/Sign.tsx
@@ -1,18 +1,49 @@
 import {
   Button,
-  Container,
   FormControl,
   FormLabel,
   Input,
   Stack,
-  Text,
+  useColorModeValue,
+  VStack,
+  Heading,
+  Box,
 } from "@chakra-ui/react";
+import bgdot from "../../assets/dotsBg.png";
 
 const SignIn = () => {
+  const bgColor = "#171941";
+  const textColor = useColorModeValue("gray.700", "gray.100");
+
+  const containerStyles = {
+    minHeight: "100vh",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundImage: `url(${bgdot})`,
+    backgroundSize: "contain",
+    backgroundPosition: "center",
+  };
+
   return (
-    <Container m={12}>
-      <Container p={12}>
-        <Stack spacing={1}>
+    <VStack
+      minHeight="100vh"
+      justifyContent="center"
+      alignItems="center"
+      bg={bgColor}
+      style={containerStyles}
+    >
+      <Box
+        p={8}
+        rounded="xl"
+        boxShadow="xl"
+        bgColor="white"
+        width="100%"
+        maxWidth="400px"
+      >
+        <Heading as="h2" size="xl" mb={6} textAlign="center" color={textColor}>
+          Sign In
+        </Heading>
+        <Stack spacing={4}>
           <FormControl>
             <FormLabel>Email address</FormLabel>
             <Input type="email" />
@@ -20,13 +51,15 @@ const SignIn = () => {
 
           <FormControl>
             <FormLabel>Password</FormLabel>
-            <Input type="email" />
+            <Input type="password" /> {/* Changed type to "password" */}
           </FormControl>
 
-          <Button size="sm">Sign In</Button>
+          <Button colorScheme="blue" size="md" width="100%">
+            Sign In
+          </Button>
         </Stack>
-      </Container>
-    </Container>
+      </Box>
+    </VStack>
   );
 };
 
